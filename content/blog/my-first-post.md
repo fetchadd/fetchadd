dd94903a68efe86e41daf8ae52b0ad5171eee68b
kaizeng <1051299241@qq.com>
2018-05-04 20:47:00 +0800

    public new content

--- content/blog/my-first-post.md
+++
@@ -1,7 +0,0 @@
----
-title: "My First Post"
-date: 2018-05-02T19:43:55+08:00
-tags: [ "Test", "Hello" ]
-draft: false
----
-
--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,10 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+git add .
+git commit -m "public new content"
+git push origin master
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
